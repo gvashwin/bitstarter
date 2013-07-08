9ab9f5ccf4b9e17a1d93ae70f76b762a1ab834e3
Ashwin <gv.ashwin@outlook.com>
2013-07-08 01:50:07 +0000

    modified web.js with toString method

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	var fs = require('fs');
 	var data = fs.readFileSync("index.html");
-	response.send(""+data);
+	response.send(""+data.toString());
 });
 
 var port = process.env.PORT || 5000;
